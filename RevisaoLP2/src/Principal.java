87feda82ffa04d6b6ef92e336d7e01a96ff0f066
hiagobrazzali <hbrazzali@gmail.com>
2020-03-10 22:01:22 -0300

    Teste

--- RevisaoLP2/src/Principal.java
+++ RevisaoLP2/src/Principal.java
@@ -8,6 +8,7 @@ public class Principal {
 		ArrayList<Produto> produtoList = new ArrayList<Produto>();
 //		ArrayList<Pessoa> pessoaList = new ArrayList<Pessoa>();
 		//
+		//
 
 		try {
 		//	pessoaList = GerenciaArquivo.lePessoas("*800");
