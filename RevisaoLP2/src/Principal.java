f81a8f35a656bededb0e582138cb6731ada0eaa3
hiagobrazzali <hbrazzali@gmail.com>
2020-03-15 22:02:14 -0300

    Adição de novos métodos.

--- RevisaoLP2/src/Principal.java
+++ RevisaoLP2/src/Principal.java
@@ -4,22 +4,27 @@ import java.util.ArrayList;
 public class Principal {
 
 	public static void main(String[] args) {
-		
+
 		ArrayList<Produto> produtoList = new ArrayList<Produto>();
 //		ArrayList<Pessoa> pessoaList = new ArrayList<Pessoa>();
 		//
 
 		try {
-		//	pessoaList = GerenciaArquivo.lePessoas("*800");
-			produtoList = GerenciaArquivo.leProduto("C:\\Users\\Hiago Brazzali\\OneDrive\\Workspace5°Periodo\\RevisaoLP2\\produtos.txt");
+			// pessoaList = GerenciaArquivo.lePessoas("*800");
+			produtoList = GerenciaArquivo
+					.leProduto("C:\\Users\\Hiago Brazzali\\OneDrive\\Workspace5°Periodo\\RevisaoLP2\\produtos.txt");
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			System.err.println("Erro ao ler arquivo");
 			e.printStackTrace();
 		}
-		//	System.out.println(produtoList.toString());
-			
-			ProdutoController.maisVendido(produtoList);
+		// System.out.println(produtoList.toString());
 
+		System.out.println("O produto mais vendido é:\n " + ProdutoController.maisVendido(produtoList).toString());
+		System.out.println("O produto com maior valor é:\n " + ProdutoController.maiorValor(produtoList).toString());
+		System.out.println("O produto com maior quantidade em estoque é:\n " + ProdutoController.maiorQtdEstoque(produtoList).toString());
+		System.out.println("O produto mais vendido é:\n " + ProdutoController.vendedorMaisVendeu(produtoList));
+		System.out.println("O produto mais vendido é:\n " + ProdutoController.extrato(produtoList, 151123950));
+		System.out.println("O produto com maior receita é:\n " + ProdutoController.maiorReceita(produtoList).toString());
 	}
 
 }
--- RevisaoLP2/src/ProdutoController.java
+++ RevisaoLP2/src/ProdutoController.java
@@ -1,25 +1,83 @@
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class ProdutoController {
-	
-	
-	public static String maisVendido(ArrayList<Produto> produto) {
-		
-	        Map<Produto,Integer> qtde = new HashMap<>();
 
-	        for(Produto prod : produto){
-	            if(qtde.containsKey(prod)){ 
-	                Integer n = qtde.get(prod);
-	                qtde.put(prod, n+1); 
-	            }else{
-	                qtde.put(prod,1);
-	            }
-	        }
-	        System.out.println(qtde);
-		
-		
-		return null;
+	public static Produto maisVendido(ArrayList<Produto> produto) {
+		Produto produto2 = new Produto();
+		for (int i = 0; i < produto.size(); i++) {
+			int maior = produto.get(0).getQtdVendida();
+
+			if (produto.get(i).getQtdVendida() > maior) {
+				maior = produto.get(i).getQtdVendida();
+				produto2 = produto.get(i);
+			}
+		}
+
+		return produto2;
+	}
+
+	public static Produto maiorValor(ArrayList<Produto> produto) {
+		Produto produto2 = new Produto();
+		for (int i = 0; i < produto.size(); i++) {
+			Double maior = produto.get(0).getValor();
+
+			if (produto.get(i).getValor() > maior) {
+				maior = produto.get(i).getValor();
+				produto2 = produto.get(i);
+			}
+		}
+		return produto2;
+	}
+
+	public static Produto maiorQtdEstoque(ArrayList<Produto> produto) {
+		Produto produto2 = new Produto();
+		for (int i = 0; i < produto.size(); i++) {
+			int maior = produto.get(0).getQuantidade();
+
+			if (produto.get(i).getQuantidade() > maior) {
+				maior = produto.get(i).getQuantidade();
+				produto2 = produto.get(i);
+			}
+		}
+		return produto2;
+	}
+
+	public static Long vendedorMaisVendeu(ArrayList<Produto> produto) {
+		Produto produto2 = new Produto();
+		for (int i = 0; i < produto.size(); i++) {
+			int maior = produto.get(0).getQtdVendida();
+
+			if (produto.get(i).getQtdVendida() > maior) {
+				maior = produto.get(i).getQtdVendida();
+				produto2 = produto.get(i);
+			}
+		}
+
+		return produto2.getVendedor();
+	}
+
+	public static String extrato(ArrayList<Produto> produto, int vendedor) {
+		String saida = "\nExtrato: ";
+		for (int i = 0; i < produto.size(); i++) {
+
+			if (produto.get(i).getVendedor() == vendedor) {
+				saida += "\n" + produto.get(i).toString();
+			}
+		}
+
+		return saida;
+	}
+
+	public static Produto maiorReceita(ArrayList<Produto> produto) {
+		Produto produto2 = new Produto();
+		for (int i = 0; i < produto.size(); i++) {
+			double maior = produto.get(0).getQtdVendida() * produto.get(0).getValor();
+
+			if (produto.get(i).getQtdVendida() * produto.get(i).getValor() > maior) {
+				maior = produto.get(i).getQtdVendida() * produto.get(i).getValor();
+				produto2 = produto.get(i);
+			}
+		}
+		return produto2;
 	}
 }
